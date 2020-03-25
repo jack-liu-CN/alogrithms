5c1f68986fd8649e21aeac886be3f17d3ecaa678
Jack <liusjHAHA@gamil.com>
2020-03-25 22:44:48 +0800

    commit counting sort

--- source/sort/counting_sort.cpp
+++ source/sort/counting_sort.cpp
@@ -1,6 +1,28 @@
 #include<iostream>
 
-void counting_sort(int* seq, int len)
+/*
+计数排序法
+	限制：数组元素为0-k区间上的整数。
+	时间复杂度：O(n)
+*/
+void counting_sort(int* A, int* B, int len, int k)
 {
-	 
+	int* C = new int[k + 1]{ 0 };
+	//C[i]保存A中i的个数
+	for (int i = 0; i < len; i++)
+	{
+		C[A[i]]++;
+	}
+	//C[i]保存A中小于等于i的个数
+	for (int i = 1; i <= k; i++)
+	{
+		C[i] += C[i - 1];
+	}
+	//B保存排好序的数组
+	for (int i = len - 1; i >= 0; i--)
+	{
+		//小于等于A[i]的个数有C[A[i]]个,所以A[i]的位置就在第C[A[i]]位
+		B[C[A[i]] - 1] = A[i];
+		C[A[i]]--;
+	}
 }
--- source/sort/insertion_sort.cpp
+++ source/sort/insertion_sort.cpp
@@ -19,35 +19,3 @@ void direct_insertion_sort(int* seq, int len)
 	}
 }
 
-/*
-二分插入排序
-	*思想：在直接插入排序的基础上，为减少比较次数，使用二分查找寻找插入位置。
-	*稳定性：稳定。
-*/
-void binary_insertion_sort(int* seq, int len)
-{
-	for (int i = 1; i < len; i++)
-	{
-		int key = seq[i];
-		int left = 0;
-		int right = i - 1;
-		while (left <= right)
-		{
-			int middle = (right + left) / 2;
-			//如果条件为seq[middle] >= key，则排序不稳定
-			if (seq[middle] > key)
-			{
-				right = middle - 1;
-			}
-			else
-			{
-				left = middle + 1;
-			}
-		}
-		for (int j = i - 1; j >= left; j--)
-		{
-			seq[j + 1] = seq[j];
-		}
-		seq[left] = key;
-	}
-}
